b8c987cce9992f6f93419aad4ec5823b2debcf7f
Laynexx-ns <argo150808@gmail.com>
2024-10-04 17:35:09 +0300

    auth v0

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -20,4 +20,60 @@ const AuthContextProvider = ({children}) => {
         }, 3600000);
         return () => clearTimeout(timer);
     }, [token]);
+
+    const onSubmit = (token) => {
+        setToken(token);
+        setShouldShowModal(false);
+    }
+
+    const onCancel = () => {
+        setShouldShowModal(false);
+    }
+
+    if (!shouldShowModal && !token) {
+        return (
+            <Result
+                status={"error"}
+                title={"Authentication failed"}
+                subTitle={"A Github token is required tp view this page"}
+                extra={[
+                    <Button
+                        type={"link"}
+                        key{"home"}
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        Try Again
+                    </Button>,
+
+                ]}
+            />
+        );
+    }
+
+    return (
+        <>
+            {shouldShowModal && (
+                <AuthModal
+                    shouldShowModal={"ShouldShowModal"}
+                    onSubmit={"onSubmit"}
+                    onCancel={"onCancel"}
+                />
+            )}
+            <AuthContext.Provider value={{token}}>{children}</AuthContext.Provider>
+        </>
+    );
+};
+
+
+export const useAuthContext = () => {
+
+    const context = useContext(AuthContext);
+    if (context === undefined){
+        throw new Error("useAuthContext must be used within a AuthContextProvider");
+    }
+    return context;
 }
+
+export default AuthContextProvider;
